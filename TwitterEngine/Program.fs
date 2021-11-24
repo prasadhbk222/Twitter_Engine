9ce54bf5ffe73b719408337ed3971f65201bf5f2
prasadhbk222 <phadkar@ufl.edu>
2021-11-24 00:49:12 -0500

    follow feature implemented

--- TwitterEngine/Program.fs
+++ TwitterEngine/Program.fs
@@ -12,17 +12,82 @@ open Akka.Remote
 open Akka.Routing
 open Akka.Actor
 open System.Threading
+open System.Collections.Generic
 
-type Instructions=
+type UsersActorInstructions=
     | RegisterAccount of string*string
     | Test
     | Login of string*string
     | Logout of string
     | PrintInfo
+    | GetFollowers of string*int
+    | Follow of string*string //userid // useridoffollowed
+
+type TweetsActorInstructions=
+    | Tweet of string*string   //userid //tweet
+    | ReTweet of string*string*string //userid //copyfromuserid //tweetid
+    | ReceiveFollowers of string*int*List<string>  //userid //tweetid //Listoffollowers
+
+type TweetsSenderActorInstruction=
+    | SendTweet of string*List<String> // tweet and recipientList
+
+let TweetsSenderActor  (twitterSystem : ActorSystem) (mailbox: Actor<_>) =
+    //printfn "abc"
+    let rec loop() = actor{
+        let! message = mailbox.Receive()
+        match message with
+        | SendTweet (tweet, recipientList) ->
+            recipientList |> Seq.iteri (fun index item -> printfn "%i: The tweet =>%s<= was sent to %s" index tweet recipientList.[index])
+
+        
+
+
+        return! loop()
+    }
+    loop()
+
+
+let TweetsActor  (twitterSystem : ActorSystem) (mailbox: Actor<_>) =
+    //printfn "abc"
+    //let mutable tweetsMap = Map.empty  // store tweetid and tweet
+    let tweetsMap = new Dictionary<int,string>()
+    //let mutable tweetsUserMap = Map.empty  // store tweetid and user(author)
+    let tweetsUserMap = new Dictionary<int, string>()
+    let mutable tweetId = 0;
+    let rec loop() = actor{
+        let! message = mailbox.Receive()
+        
+        match message with
+        | Tweet (userId , tweet) ->
+            tweetId <- tweetId + 1
+            //tweetsMap <- tweetsMap.Add(userId, tweet)
+            tweetsMap.Add(tweetId, tweet)
+            //tweetsUserMap <- tweetsUserMap.Add(tweetId, userId)
+            tweetsUserMap.Add(tweetId, userId)
+            // get folllowers of userid
+            let actorPath =  @"akka://twitterSystem/user/usersRef"
+            let usersRef = select actorPath twitterSystem
+            usersRef <! GetFollowers(userId, tweetId)
+
+        | ReceiveFollowers (userId, tweetId, followerList) ->
+            let actorPath =  @"akka://twitterSystem/user/tweetsSenderRef"
+            let tweetsSenderRef = select actorPath twitterSystem
+            let tweet = tweetsMap.[tweetId]
+            tweetsSenderRef <! SendTweet(tweet, followerList)
+
+        | _-> 
+            printfn "Wrong input"
+
+
+
+        return! loop()
+    }
+    loop()
+
 
 
 let TwitterServer  (twitterSystem : ActorSystem) (mailbox: Actor<_>) =
-    printfn "abc"
+    //printfn "abc"
     let rec loop() = actor{
         let! message = mailbox.Receive()
 
@@ -34,16 +99,24 @@ let TwitterServer  (twitterSystem : ActorSystem) (mailbox: Actor<_>) =
     loop()
 
 let UsersActor (twitterSystem : ActorSystem) (mailbox: Actor<_>) =
-    let mutable userPasswordMap = Map.empty
+    //let mutable userPasswordMap = Map.empty
+    let userPasswordMap = new Dictionary<string,string>()
     let mutable activeUsersSet = Set.empty
+    let mutable userFollowerList = new Dictionary<string, List<string>>()
     let rec loop() = actor {
         let! message = mailbox.Receive()
+        let sender = mailbox.Sender()
 
         match message with 
         | RegisterAccount (userid, password) ->
             printfn "%s %s" userid password
             // Hash the password later
-            userPasswordMap <- userPasswordMap.Add(userid, password)
+            let followerList = new List<string>() 
+            //userFollowerList <- userFollowerList.Add(userid, followerList)
+            userFollowerList.Add(userid, followerList)
+            userPasswordMap.Add(userid, password)
+            //userPasswordMap <- userPasswordMap.Add(userid, password)
+            
  
 
         | Login (userid, password) ->
@@ -61,15 +134,30 @@ let UsersActor (twitterSystem : ActorSystem) (mailbox: Actor<_>) =
             activeUsersSet <- activeUsersSet.Remove(userid)
             // destroy token at the client side
 
+        | Follow (userId, userIdOfFollowed) ->
+            let followerList = userFollowerList.[userIdOfFollowed]
+            followerList.Add(userId)
+
+        | GetFollowers (userId, tweetId) ->
+            let actorPath =  @"akka://twitterSystem/user/tweetsRef"
+            let tweetsRef = select actorPath twitterSystem
+            tweetsRef <! ReceiveFollowers(userId, tweetId, userFollowerList.[userId])
+
+
+
+
+
         | PrintInfo ->
             printfn "Active users are %A" activeUsersSet
             printfn "All users are %A" userPasswordMap
+            printfn "User followers lists are %A" userFollowerList
+            printfn "%A" mailbox.Self.Path
 
-
-
-        
         | Test ->
             printfn "test"
+
+        | _-> 
+            printfn "Wrong input"
             
 
 
@@ -86,12 +174,17 @@ let main argv =
     let nodes = (int) argv.[0];
     let twitterSystem = ActorSystem.Create("twitterSystem");
     let twitterServerRef = spawn twitterSystem "twitterServerRef" (TwitterServer twitterSystem);
+    let tweetsSenderRef = spawn twitterSystem "tweetsSenderRef" (TweetsSenderActor twitterSystem);
     let usersRef = spawn twitterSystem "usersRef" (UsersActor twitterSystem);
+    let tweetsRef = spawn twitterSystem "tweetsRef" (TweetsActor twitterSystem);
+
     usersRef <! RegisterAccount("prasad", "prasad")
     usersRef <! RegisterAccount("vaishnavi", "vaishnavi")
     usersRef <! Login("prasad", "prasad")
     usersRef <! Login("vaishnavi", "vaishnavi")
-    usersRef <! Logout("prasad")
+    usersRef <! Follow("vaishnavi", "prasad")
+    tweetsRef <! Tweet("prasad", "Hello followers...gooooood Morning")
+    //usersRef <! Logout("prasad")
     usersRef <! PrintInfo
     System.Console.ReadKey() |> ignore
     0
